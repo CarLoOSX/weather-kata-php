510dcdbf8464955abf913edf68d3381cf40f9e9d
Carlos Martinez <carloosx@MacBook-Pro.local>
2020-10-28 22:07:27 +0100

    no runing test

--- src/Forecast.php
+++ src/Forecast.php
@@ -6,32 +6,39 @@ use GuzzleHttp\Client;
 
 class Forecast
 {
-    public function predict(string &$city, \DateTime $datetime = null, bool $wind = false): string
-    {
-        // When date is not provided we look for the current prediction
-        if (!$datetime) {
-            $datetime = new \DateTime();
-        }
 
+     const DAYSCONSTANT = "+6 days 00:00:00";
+    const LOCATIONCONSTANT = "https://www.metaweather.com/api/location/search/?query=";
+    const SEARCHCONSTANT = "https://www.metaweather.com/api/location/";
+    const DATEFORMATCONSTANT = 'Y-m-d';
+
+   public function __construct() {
+
+    }
+
+    public function predict(string &$city, \DateTime $datetime = null, bool $wind = false)
+    {
         // If there are predictions
-        if ($datetime < new \DateTime("+6 days 00:00:00")) {
+        if ($datetime >= new \DateTime(self::DAYCONSTANT))
+            return "";
+        else $datetime = new \DateTime();
 
 
             // Create a Guzzle Http Client
             $client = new Client();
 
             // Find the id of the city on metawheather
-            $woeid = json_decode($client->get("https://www.metaweather.com/api/location/search/?query=$city")->getBody()->getContents(),
+            $woeid = json_decode($client->get(self::SEARCHCONSTANT.$city)->getBody()->getContents(),
                 true)[0]['woeid'];
             $city = $woeid;
 
             // Find the predictions for the city
-            $results = json_decode($client->get("https://www.metaweather.com/api/location/$woeid")->getBody()->getContents(),
+            $results = json_decode($client->get(self::LOCATIONCONSTANT.$woeid)->getBody()->getContents(),
                 true)['consolidated_weather'];
             foreach ($results as $result) {
 
                 // When the date is the expected
-                if ($result["applicable_date"] == $datetime->format('Y-m-d')) {
+                if ($result["applicable_date"] == $datetime->format(self::DATEFORMATCONSTANT)) {
                     // If we have to return the wind information
                     if ($wind) {
                         return $result['wind_speed'];
@@ -40,8 +47,6 @@ class Forecast
                     }
                 }
             }
-        } else {
-            return "";
-        }
+
     }
 }
